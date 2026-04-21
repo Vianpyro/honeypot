d8657803c0f1fad30308b2cd7457bee9b0193672
Vianpyro <vianney+github@veremme.org>
2026-04-21 09:49:44 -0400

    Stop tracking generated content.js

--- cloudflare/content.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-//  content.js — Fake content generators
-//  Contains: fictional credentials
-//  Make your own fake content here,
-//      but avoid using real sensitive data
-//      to prevent accidental disclosure.
-// ============================================================
